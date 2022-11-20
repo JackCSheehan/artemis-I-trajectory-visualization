32ab76cfdefecf5813cbddc54a1346eaffb78786
Jack Sheehan <31775474+JackCSheehan@users.noreply.github.com>
2022-11-20 15:41:35 -0600

    Implemented proper animations and moon positions

--- src/constants.py
+++ src/constants.py
@@ -5,3 +5,19 @@ MOON_RADIUS = 1740
 # Colors
 EARTH_COLOR = "dodgerblue"
 MOON_COLOR = "gainsboro"
+ORION_TRACE_COLOR = "red"
+TELEMETRY_PLOT_COLORS = "lime"
+SPACE_COLOR = "black"
+BG_COLOR = "black"
+FG_COLOR = "white"
+
+# Other plotting constants
+MAX_DISTANCE = 450000   # Used for scaling distance plot
+MAX_VELOCITY = 8        # Used for scaling velocity plot
+BODY_RESOLUTION = 15    # Resolution for drawing Earth and Moon
+MAX_X = 1200000
+MIN_X = -1200000
+MAX_Y = 1000000
+MIN_Y = -1000000
+
+MOON_ERASER_INCREASE = 2000 # How many units of distance to add to moon's radius for moon eraser patch
